7a2ba8c081c840400c07146857db2e604c43b8f5
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-12 16:31:33 +0200

    create /bacheca

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// cartella public contenente gli asset statici
 app.use(express.static('public'));
 
+// * ARRAY CONTENENTE I POST
 const posts = [
   {
     titolo: "post 1",
@@ -36,13 +38,16 @@ const posts = [
     tags: ["dolce", "dessert"]
   }
 ];
-
-
-
+// * ROTTA PRINCIPALE
 app.get("/", (req, res)=>{
     res.send("Server del mio blog");
 });
+// * ROTTA /bacheca
+app.get('/bacheca', (req, res) => {
+  res.json({ posts });
+});
 
+// * AVVIO SERVER
 app.listen(port, ()=>{
     console.log("il server è in ascolto sulla porta:" + port); 
 });
