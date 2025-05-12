23b5caae3fda68804d0719bea9a1cfce1d341516
andrea-mosca <andrea.moscateli@gmail.com>
2025-05-12 16:50:55 +0200

    create posts.js file

--- app.js
+++ app.js
@@ -6,38 +6,7 @@ const port = 3000;
 app.use(express.static('public'));
 
 // * ARRAY CONTENENTE I POST
-const posts = [
-  {
-    titolo: "post 1",
-    contenuto: "ciambellone",
-    immagine: "/images/ciambellone.jpeg",
-    tags: ["dolce", "colazione"]
-  },
-  {
-    titolo: "post 2",
-    contenuto: "Cracker alla barbabietola",
-    immagine: "/images/cracker-barbabietola.jpeg",
-    tags: ["salato", "merenda", "barbabietola", "snack"]
-  },
-  {
-    titolo: "post 3",
-    contenuto: "pane fritto dolce",
-    immagine: "/images/pane-fritto-dolce.jpeg",
-    tags: ["dolce", "fritto", "pane", "dessert"]
-  },
-  {
-    titolo: "post 4",
-    contenuto: "pasta alla barbabietola",
-    immagine: "/images/pasta-barbabietola.jpeg",
-    tags: ["primo", "barbabietola", "salato"]
-  },
-  {
-    titolo: "post 5",
-    contenuto: "torta paesana",
-    immagine: "/images/torta-paesana.jpeg",
-    tags: ["dolce", "dessert"]
-  }
-];
+const posts = require("./posts");
 // * ROTTA PRINCIPALE
 app.get("/", (req, res)=>{
     res.send("Server del mio blog");
---
+++ posts.js
@@ -0,0 +1,33 @@
+const posts = [
+  {
+    titolo: "post 1",
+    contenuto: "ciambellone",
+    immagine: "/images/ciambellone.jpeg",
+    tags: ["dolce", "colazione"]
+  },
+  {
+    titolo: "post 2",
+    contenuto: "Cracker alla barbabietola",
+    immagine: "/images/cracker-barbabietola.jpeg",
+    tags: ["salato", "merenda", "barbabietola", "snack"]
+  },
+  {
+    titolo: "post 3",
+    contenuto: "pane fritto dolce",
+    immagine: "/images/pane-fritto-dolce.jpeg",
+    tags: ["dolce", "fritto", "pane", "dessert"]
+  },
+  {
+    titolo: "post 4",
+    contenuto: "pasta alla barbabietola",
+    immagine: "/images/pasta-barbabietola.jpeg",
+    tags: ["primo", "barbabietola", "salato"]
+  },
+  {
+    titolo: "post 5",
+    contenuto: "torta paesana",
+    immagine: "/images/torta-paesana.jpeg",
+    tags: ["dolce", "dessert"]
+  }
+];
+module.exports = posts;
